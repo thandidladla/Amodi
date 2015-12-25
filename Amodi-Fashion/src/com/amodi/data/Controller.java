1c3da5997cce57328f74cec8472114e298f03605
Mohammad Kohankhaki <m.kohankhaki@gmail.com>
2015-12-25 22:14:14 +0100

    executeQuery(),connect(), some little changes

--- Amodi-Fashion/src/com/amodi/data/Controller.java
+++ Amodi-Fashion/src/com/amodi/data/Controller.java
@@ -1,11 +1,12 @@
 package com.amodi.data;
-//test
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSetMetaData;
 import java.util.Vector;
 
 public class Controller {
@@ -15,40 +16,24 @@ public class Controller {
 	private ResultSet resultSet = null;
 	private String url, username, password;
 
-	public static final String[] KLEIDUNGSART = { "Kleidungsart",
-			"KleidungsID", "Kleidungsart", "Form" };
-	public static final String[] GESCHAEFT = { "Geschaeft", "GeschaeftsID",
-			"Name", "Link", "Straße", "PLZ", "Ort" };
-	public static final String[] USER = { "User", "Username", "Password",
-			"E-Mail", "PLZ", "Ort" };
-	public static final String[] ARTIKEL = { "Artikel", "ArtikelID",
-			"Geschlecht", "Marke", "Farbe", "Tags", "Stil", "", "KleidungsID" };
-	public static final String[] ANGEBOT = { "Angebot", "AngebotID",
-			"Startdatum", "Ablaufdatum", "Status", "Preis", "Angebotsart",
-			"ArtikelID", "GeschaeftsID" };
+	public static final String[] KLEIDUNGSART = { "Kleidungsart", "KleidungsID", "Kleidungsart", "Form" };
+	public static final String[] GESCHAEFT = { "Geschaeft", "GeschaeftsID", "Name", "Link", "Straße", "PLZ", "Ort" };
+	public static final String[] USER = { "User", "Username", "Password", "E-Mail", "PLZ", "Ort" };
+	public static final String[] ARTIKEL = { "Artikel", "ArtikelID", "Geschlecht", "Marke", "Farbe", "Tags", "Stil", "Bilder",
+			"KleidungsID" };
+	public static final String[] ANGEBOT = { "Angebot", "AngebotID", "Startdatum", "Ablaufdatum", "Status", "Preis",
+			"Angebotsart", "ArtikelID", "GeschaeftsID" };
 
 	public Controller() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connect = DriverManager.getConnection(
-					"jdbc:mysql://localhost/a9691386_Amodi", "mithras",
-					"Pas413839");
-			statement = connect.createStatement();
-			System.out.println("established");
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		connect();
 	}
 
-	public boolean edit(Object primkey_value, Object changed_value,
-			int changed_index, String[] relation) {
-		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART)
-				|| relation.equals(GESCHAEFT) || relation.equals(ANGEBOT)
-				|| relation.equals(USER)) {
+	public boolean edit(Object primkey_value, Object changed_value, int changed_index, String[] relation) {
+		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART) || relation.equals(GESCHAEFT)
+				|| relation.equals(ANGEBOT) || relation.equals(USER)) {
 			String query = "";
 
-			query = "UPDATE " + relation[0] + " SET " + relation[changed_index+1]
-					+ "= '" + changed_value + "' ";
+			query = "UPDATE " + relation[0] + " SET " + relation[changed_index + 1] + "= '" + changed_value + "' ";
 
 			query += "WHERE " + relation[1] + "='" + primkey_value + "';";
 			try {
@@ -65,8 +50,7 @@ public class Controller {
 
 	public boolean exists(Object[] row, String[] relation) {
 		String query = "SELECT * FROM ";
-		query += relation[0] + " WHERE " + relation[1] + "=" + "'"
-				+ (String) row[0] + "';";
+		query += relation[0] + " WHERE " + relation[1] + "=" + "'" + (String) row[0] + "';";
 		try {
 			resultSet = statement.executeQuery(query);
 		} catch (SQLException e) {
@@ -78,28 +62,13 @@ public class Controller {
 
 	public Object[][] loadData(String[] relation) {
 		String query = "SELECT * FROM " + relation[0];
-		try {
-			resultSet = statement.executeQuery(query);
-			resultSet.last();
-			Object[][] data = new Object[resultSet.getRow()][relation.length - 1];
-			resultSet.beforeFirst();
-			for (int i = 0; resultSet.next(); i++) {
-				for (int j = 1; j < relation.length; j++) {
-					data[i][j - 1] = resultSet.getObject(j);
-				}
-			}
-			return data;
-		} catch (SQLException e) {
-			// Exception Handling
-			System.out.println(e);
-			return null;
-		}
+		return executeQuery(query);
+
 	}
 
 	public boolean add(Object[] row, String[] relation) {
-		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART)
-				|| relation.equals(GESCHAEFT) || relation.equals(ANGEBOT)
-				|| relation.equals(USER)) {
+		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART) || relation.equals(GESCHAEFT)
+				|| relation.equals(ANGEBOT) || relation.equals(USER)) {
 			String query1 = "", query2 = "";
 			if (!exists(row, relation)) {
 				for (int i = 0; i < relation.length; i++) {
@@ -108,7 +77,7 @@ public class Controller {
 						query2 = "VALUES (";
 					} else if (i != 1) {
 						query1 += relation[i];
-						query2 += "'" + (String) row[i-1] + "'";
+						query2 += "'" + (String) row[i - 1] + "'";
 						if (i != relation.length - 1) {
 							query1 += ",";
 							query2 += ",";
@@ -133,12 +102,10 @@ public class Controller {
 	}
 
 	public boolean remove(Vector<Object> row, String[] relation) {
-		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART)
-				|| relation.equals(GESCHAEFT) || relation.equals(ANGEBOT)
-				|| relation.equals(USER)) {
+		if (relation.equals(ARTIKEL) || relation.equals(KLEIDUNGSART) || relation.equals(GESCHAEFT)
+				|| relation.equals(ANGEBOT) || relation.equals(USER)) {
 			String query = "DELETE FROM ";
-			query += relation[0] + " WHERE " + relation[1] + "=" + "'"
-					+ (String) row.get(0) + "'" + ";";
+			query += relation[0] + " WHERE " + relation[1] + "=" + "'" + (String) row.get(0) + "'" + ";";
 			try {
 				statement.executeQuery(query);
 				return true;
@@ -150,4 +117,50 @@ public class Controller {
 			return false;
 		}
 	}
+
+//	public Object[][] search() {
+//
+//	}
+
+	public Object[][] executeQuery(String sql) {
+		try {
+			resultSet = statement.executeQuery(sql);
+			if (resultSet == null) {
+				return null;
+			} else {
+				Object[][] result;
+				ResultSetMetaData meta = resultSet.getMetaData();
+
+				resultSet.last();
+				int rows = resultSet.getRow();
+				int columns = meta.getColumnCount();
+				resultSet.beforeFirst();
+				result = new Object[rows][columns];
+
+				for (int i = 0; resultSet.next(); i++) {
+					for (int j = 1; j < columns; j++) {
+						result[i][j-1] = resultSet.getObject(j);
+					}
+				}
+				return result;
+			}
+		} catch (SQLException e) {
+			// Exception Handling
+			return null;
+		}
+	}
+	
+	public boolean connect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://localhost/a9691386_Amodi", "mithras", "Pas413839");
+			statement = connect.createStatement();
+			System.out.println("established");
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
+	}
+
 }
